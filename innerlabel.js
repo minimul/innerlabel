78c291259848309acfa278655393699d6b670c00
Christian <minimul@simplton.com>
2011-10-16 22:47:54 -0400

    Needed to overhaul because of my lack of understanding with respect to writing good plugin code

--- innerlabel.js
+++ innerlabel.js
@@ -2,30 +2,49 @@
  *  and is freely distributable under the terms of an MIT-style license.
 /*--------------------------------------------------------------------------*/
 (function($){
-  $.fn.innerlabel = function(options){
-    var defaults = { labelColor: 'gray', focusColor: 'black' };
-    var settings = $.extend(defaults,options);
-    var labelText = this.attr('data-label');
-    if(!this.val() || labelText == this.val()){
-      this.val(labelText); 
-      this.css({ color: settings.labelColor });
-    }
-    var el = this;
-    this.bind({
-      focus: function(){
-        if(labelText == el.val()){
-          el.val('');
-          el.css({ color: settings.focusColor });
+
+  var methods = {
+    init: function(options) { 
+      this.data('innerlabel', $.extend({ labelColor: 'gray', focusColor: 'black' },options)); 
+      var settings = this.data('innerlabel');
+      return this.each(function(){
+        var $this = $(this);
+        var labelText = $this.attr('data-label');
+        if(!$this.val() || labelText == $this.val()){
+          $this.val(labelText); 
+          $this.css({ color: settings.labelColor });
         }
-      },
-      blur: function(){
-        if(!el.val()){
-          el.val(labelText);
-          el.css({ color: settings.labelColor });
-        }
+        $this.bind({ focus: methods.focus, blur: methods.blur });
+      });
+    },
+    focus: function(){
+      var $this = $(this);
+      var settings = $this.data('innerlabel');
+      if($this.attr('data-label') == $this.val()){
+        $this.val('');
+        $this.css({ color: settings.focusColor });
       }
-    });
-    return this; 
+    },
+    blur: function(){
+      var $this = $(this);
+      var settings = $this.data('innerlabel');
+      if(!$this.val()){
+        $this.val($this.attr('data-label'));
+        $this.css({ color: settings.labelColor });
+      }
+    }
   };
+
+  $.fn.innerlabel = function(method){
+    // Method calling logic
+    if(methods[method]){
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    }else if(typeof method === 'object' || ! method){
+      return methods.init.apply(this, arguments);
+    }else{
+      $.error( 'Method ' +  method + ' does not exist on jQuery.innerlabel' );
+    }    
+  };
+ 
 })(jQuery);
 
